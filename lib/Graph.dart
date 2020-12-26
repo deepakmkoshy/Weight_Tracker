8aa309ab50d19eb377fd137d1edd4f10bdc8b1b5
Deepak <deepakmkoshy@gmail.com>
2020-12-26 18:26:31 +0530

    Trying to implement wt track with Firebase

---
+++ lib/Graph.dart
@@ -0,0 +1,194 @@
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class Graph extends StatefulWidget {
+  var wt = <String>[];
+
+  Graph(this.wt);
+
+  @override
+  GraphState createState() => GraphState();
+}
+
+class GraphState extends State<Graph> {
+  List<Color> gradientColors = [
+    const Color(0xff23b6e6),
+    const Color(0xff02d39a),
+  ];
+
+  double minY;
+  double maxY;
+  double maxX;
+  double interval;
+  double intervalX;
+
+  @override
+  void initState() {
+    super.initState();
+    minY = 60;
+    maxY = 100;
+    interval = 5;
+    intervalX = 1;
+  }
+
+  void clearGraph() {
+    setState(() {
+      minY = 60;
+      maxY = 100;
+      interval = 5;
+      intervalX = 1;
+    });
+  }
+
+  //Function to create the Spots from scratch using the weight list
+  List<FlSpot> _updateSta() {
+    final fSpots = <FlSpot>[];
+    if (widget.wt.isNotEmpty) {
+      for (int i = 0; i < widget.wt.length; i++) {
+        double val = double.parse(widget.wt[i]);
+
+        fSpots.add(FlSpot((i.toDouble()), val));
+        if (i == 0) {
+          maxY = (val + 20).roundToDouble();
+          if (val <= 20) {
+            minY = 0;
+          }
+          else{
+          minY = (val - 20).roundToDouble();
+
+          }
+        }
+        if (val > maxY) //Updating Y-axis according to max wt
+          maxY = val + 5;
+        else if (val < minY) minY = val - 5;
+        maxX = widget.wt.length.toDouble() - 1;
+        //Updates X-axis according to input instead of standard 7 data
+
+//Updates the interval in Y-axis so as to avoid clustering of sidetitles
+        if (maxY - minY >= 100)
+          interval = 11;
+        else if (maxY - minY >= 60)
+          interval = 7;
+        else
+          interval = 5;
+
+        //Updates the interval in Y-axis so as to avoid clustering of sidetitles
+        print(maxX);
+        if (maxX >= 7)
+          intervalX = 2;
+        else
+          intervalX = 1;
+      }
+      return fSpots;
+    } else {
+      minY = 60;
+      maxY = 100;
+      maxX = 0;
+      interval = 5;
+      intervalX = 1;
+      return [FlSpot(0, 0)]; //Placeholder until the user inputs the weight
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: AspectRatio(
+        aspectRatio: 1,
+        child: LineChart(graphData()),
+      ),
+    );
+  }
+
+  LineChartData graphData() {
+    return LineChartData(
+      axisTitleData: FlAxisTitleData(
+        leftTitle: AxisTitle(
+            showTitle: true,
+            titleText: "(Kg)",
+            reservedSize: 10,
+            margin: 5,
+            textStyle: TextStyle(color: Colors.black, fontSize: 11),
+            textAlign: TextAlign.end),
+      ),
+      clipData: FlClipData.all(),
+      gridData: FlGridData(
+        show: false,
+      ),
+      lineBarsData: [
+        LineChartBarData(
+          spots: _updateSta(),
+          //          spots: [
+//            FlSpot(0, 90),
+//            FlSpot(1, 80),
+//            FlSpot(2, 70),
+//            FlSpot(3, 78),
+//            FlSpot(4, 86),
+//            FlSpot(5, 64),
+//            FlSpot(6, 70),
+//          ],
+          //  showingIndicators: showIndexes,
+          isCurved: true,
+          colors: gradientColors,
+          barWidth: 5,
+          isStrokeCapRound: true,
+          dotData: FlDotData(
+            show: true,
+          ),
+          belowBarData: BarAreaData(
+            show: true,
+            colors:
+                gradientColors.map((color) => color.withOpacity(0.3)).toList(),
+          ),
+        ),
+      ],
+      titlesData: FlTitlesData(
+        show: true,
+        bottomTitles: SideTitles(
+          showTitles: true,
+          reservedSize: 22,
+          getTextStyles: (value) => const TextStyle(
+              color: Color(0xff68737d),
+              fontWeight: FontWeight.bold,
+              fontSize: 16),
+          interval: intervalX,
+
+          ///Leaving this here to understand the format to edit the X-axis later
+          //getTitles: (value) {},
+
+//                  case 1:
+//                    return 'MA;
+//                  case 5:
+//                    return 'JUN';
+//                  case 8:
+//                    return 'SEP';
+//                  case 10:
+//                    return 'OCT';
+//                }
+//                return '';
+//              },
+          margin: 8,
+        ),
+        leftTitles: SideTitles(
+          showTitles: true,
+          getTextStyles:(value) => const TextStyle(
+            color: Color(0xff67727d),
+            fontWeight: FontWeight.bold,
+            fontSize: 15,
+          ),
+          reservedSize: 22,
+          margin: 6,
+          interval: interval,
+        ),
+      ),
+      borderData: FlBorderData(
+          show: true,
+          border: Border.all(color: const Color(0xff37434d), width: 1)),
+      minX: 0,
+      maxX: maxX,
+      minY: minY,
+      maxY: maxY,
+    );
+  }
+}
---
+++ lib/HomePage.dart
@@ -0,0 +1,188 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:wtfbtest/Graph.dart';
+// import 'package:hive/hive.dart';
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var weight = <String>[];
+  bool _showValidationError = false;
+  final _controller = TextEditingController();
+  String _errorMsg;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // if (box.isNotEmpty) //Updating the local weight list from the database
+    //   weight = box.get('weight');
+    // else
+    //   box.put('weight', weight);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _updateInput() {
+    setState(() {
+      String input = _controller.text;
+      print(input);
+      try {
+        double d = double.parse(input);
+        _showValidationError = false;
+        if (d < 0 || d > 200) {
+          _showValidationError = true;
+          _errorMsg = "Number should be less than 200";
+        } else {
+          weight.add(input);
+
+          // box.put('weight', weight);
+        }
+      } on Exception catch (e) {
+        print('Error: $e');
+        _errorMsg = "Invalid number entered";
+
+        _showValidationError = true;
+      }
+    });
+  }
+
+  String _viewList() {
+    String tmp = '';
+    if (weight.isNotEmpty) {
+      for (var text in weight) {
+        tmp = tmp + ' ' + text;
+      }
+    }
+    return tmp;
+  }
+
+  //Clearing the Hive weight List
+  void clearDB() {
+    setState(() {
+      weight.clear();
+      _controller.clear();
+      // box.clear();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("WT Tracker-Firebase"),centerTitle: true),
+      
+          body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: <Widget>[
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Expanded(
+                    child: TextField(
+                      controller: _controller,
+                      style: Theme.of(context).textTheme.headline5,
+                      decoration: InputDecoration(
+                        labelText: "Enter your current weight",
+                        errorText: _showValidationError ? _errorMsg : null,
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5.0)),
+                      ),
+                      keyboardType: TextInputType.number,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Ink(
+                      decoration: ShapeDecoration(
+                        shadows: [
+                          BoxShadow(
+                              color: Color(0x40000000),
+                              blurRadius: 4,
+                              offset: Offset(0, 4))
+                        ],
+                        shape: CircleBorder(),
+                        gradient: LinearGradient(
+                          colors: <Color>[
+                            Colors.purple,
+                            Colors.deepPurpleAccent,
+                            // Colors.deepOrange,
+                          ],
+                        ),
+                      ),
+                      child: IconButton(
+                        onPressed: () {
+                          _updateInput();
+                          _controller.clear();
+                        },
+                        icon: Icon(Icons.add, color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                child: Container(
+                  child: InputDecorator(
+                    child: Text(_viewList(),
+                        style: Theme.of(context).textTheme.headline5),
+                    decoration: InputDecoration(
+                      labelText: 'The weight list',
+                      labelStyle: Theme.of(context).textTheme.headline5,
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5.0)),
+                    ),
+                  ),
+                ),
+              ),
+              Graph(weight),
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: FlatButton(
+                  onPressed: clearDB,
+                  padding: EdgeInsets.all(0.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0),
+                      boxShadow: [
+                        BoxShadow(
+                            color: Color(0x40000000),
+                            blurRadius: 4,
+                            offset: Offset(0, 4))
+                      ],
+                      gradient: LinearGradient(
+                        colors: <Color>[
+                          Colors.purple,
+                          Colors.deepPurpleAccent,
+                          // Colors.deepOrange,
+                        ],
+                      ),
+                    ),
+                    padding: EdgeInsets.all(10.0),
+                    child: Text(
+                      "CLEAR",
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline5
+                          .copyWith(color: Colors.white, fontSize: 25.0),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
